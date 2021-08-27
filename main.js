29562e624168f992e0aee1c8138961304dd33a94
arnav770 <77731681+arnav770@users.noreply.github.com>
2021-08-27 12:44:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+status = "";
+video = "";
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+
+function start() {
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    objectname = document.getElementById("objectName").value;
+}
+
+function modelLoaded() {
+    console.log("modelLoaded");
+    status = true;
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+}
